d44f1de3f35bb880eed9bf570a0facdf03845ce1
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-08-12 17:34:58 +0530

    Time: 93 ms (40.26%), Space: 10.9 MB (15.12%) - LeetHub

--- 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
+++ 1895-minimum-number-of-operations-to-move-all-balls-to-each-box/1895-minimum-number-of-operations-to-move-all-balls-to-each-box.cpp
@@ -4,9 +4,10 @@ public:
         ios::sync_with_stdio(false);
         cin.tie(nullptr);
         vector<int>v;
-        for(int i=0;i<boxes.length();i++){
+        int n=boxes.length();
+        for(int i=0;i<n;i++){
             int count=0;
-            for(int j=0;j<boxes.length();j++){
+            for(int j=0;j<n;j++){
                 if(boxes[j]=='1'){
                 count=count+abs(i-j);
                 }
