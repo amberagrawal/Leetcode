0ee72fe4201a5dbaf15abba88b30aba12406450e
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-08-13 22:54:00 +0530

    Time: 27 ms (96.7%), Space: 51 MB (8.61%) - LeetHub

--- 2402-maximum-xor-after-operations/2402-maximum-xor-after-operations.cpp
+++ 2402-maximum-xor-after-operations/2402-maximum-xor-after-operations.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maximumXOR(vector<int>& nums) {
+      ios::sync_with_stdio(false);
+        cin.tie(nullptr);  
       int ans=0;
       for(int i=0;i<nums.size();i++){
         ans=ans|nums[i];
