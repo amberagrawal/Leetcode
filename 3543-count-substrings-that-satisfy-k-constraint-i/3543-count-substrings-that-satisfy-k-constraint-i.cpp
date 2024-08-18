4ef39ef369232df8d83425c5c5f217114ed0fd07
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-08-18 18:23:49 +0530

    Time: 89 ms (6.25%), Space: 31.5 MB (6.25%) - LeetHub

---
+++ 3543-count-substrings-that-satisfy-k-constraint-i/3543-count-substrings-that-satisfy-k-constraint-i.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int count1(string s){
+        int a=0;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='1'){
+                a++;
+            }
+        }
+        return a;
+    }
+    int countKConstraintSubstrings(string s, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
+        int count=0;
+        for(int i=1;i<=s.length();i++){
+            for(int j=0;j<=s.length()-i;j++){
+              string sub=s.substr(j,i);
+              if(count1(sub)<=k||sub.length()-count1(sub)<=k){
+                count++;
+              }
+            }
+        }
+        return count;
+    }
+};
