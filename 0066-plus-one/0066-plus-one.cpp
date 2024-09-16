6c35a3bbcbe9c4a8061ea672df06754ab6e41b38
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-16 12:15:23 +0530

    Time: 2 ms (43.54%), Space: 10.5 MB (81.39%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         for(int i=digits.size()-1;i>=0;i--){
             if(digits[i]==9&&i==0){
                 digits[i]=0;
