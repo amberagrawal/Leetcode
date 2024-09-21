03720972fdf5d3d28ff4e8a13df65c8f35e5375a
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-21 23:48:56 +0530

    Time: 35 ms (99.3%), Space: 77.2 MB (17.23%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         long long total=0;
         for(int i=0;i<chalk.size();i++){
             total=total+chalk[i];
