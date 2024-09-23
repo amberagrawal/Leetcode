498a04cef02d10ca8ecc839deaf107049b333e13
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-23 19:25:48 +0530

    Time: 91 ms (99.22%), Space: 85.7 MB (93.23%) - LeetHub

--- 0164-maximum-gap/0164-maximum-gap.cpp
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maximumGap(vector<int>& nums) {
+     ios::sync_with_stdio(false);
+     cin.tie(nullptr);
       sort(nums.begin(),nums.end());
       int diff=0;
       for(int i=1;i<nums.size();i++) {
