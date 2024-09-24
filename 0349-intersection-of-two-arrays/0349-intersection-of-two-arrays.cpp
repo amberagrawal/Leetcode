670072098fbe8d3f850c793dc850bee21afb2392
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-24 23:21:18 +0530

    Time: 13 ms (5.48%), Space: 13 MB (65.98%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         set<int>s;
         for(int i=0;i<nums1.size();i++){
             for(int j=0;j<nums2.size();j++){
