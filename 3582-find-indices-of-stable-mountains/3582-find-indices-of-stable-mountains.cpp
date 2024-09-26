e7d49158820445d1aaeeff98430805c6d6b1f62d
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-26 23:25:44 +0530

    Time: 12 ms (14.77%), Space: 27.6 MB (45.55%) - LeetHub

--- 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.cpp
+++ 3582-find-indices-of-stable-mountains/3582-find-indices-of-stable-mountains.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> stableMountains(vector<int>& height, int threshold) {
+      ios::sync_with_stdio(false);
+      cin.tie(nullptr);
        vector<int>v;
        for(int i=1;i<height.size();i++){
         if(height[i-1]>threshold){
