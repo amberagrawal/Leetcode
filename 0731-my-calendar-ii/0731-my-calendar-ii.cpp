ea080c475f00918678d34fd40ae99a25cb182ae3
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-27 13:58:47 +0530

    Time: 167 ms (26.38%), Space: 42.7 MB (11.57%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.cpp
+++ 0731-my-calendar-ii/0731-my-calendar-ii.cpp
@@ -3,17 +3,13 @@ public:
     map<int, int> mp;
 
     bool book(int start, int end) {
-        // Mark the start of an event and the end of an event
         mp[start]++;
         mp[end]--;
         
         int active_events = 0;
-        
-        // Traverse through the time points and check if any point has more than 2 events overlapping
         for (auto it : mp) {
             active_events += it.second;
             if (active_events >= 3) {
-                // Revert changes as this booking cannot be added
                 mp[start]--;
                 mp[end]++;
                 return false;
