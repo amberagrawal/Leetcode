9bc7abb2bf8ef79b7b7bda6b2bf6313e8ed57c7c
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-28 12:06:33 +0530

    Time: 7 ms (26.32%), Space: 25.8 MB (22.7%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addStrings(string a, string b) {
+        if(a.length()>b.length()){
+            while(a.length()!=b.length()){
+                b='0'+b;
+            }
+        }
+        else{
+           while(a.length()!=b.length()){
+                a='0'+a;
+            } 
+        }
+        int carry=0;
+        string s(a.length(),'0');
+        for(int i=a.length()-1;i>=0;i--){
+            int sum=(a[i]-'0')+(b[i]-'0')+carry;
+                s[i]=(sum%10)+'0';
+                carry=sum/10;
+        }
+        if(carry > 0) {
+            s = '1' + s;
+        }
+        return s;
+    }
+};
