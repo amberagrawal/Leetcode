cc24e39ed2096e5e1c597651e9ac029a1f2e6e09
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-29 02:38:15 +0530

    Time: 0 ms (100%), Space: 7.8 MB (78.87%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int trailingZeroes(int n) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         int count=0;
         while(n>0){
             count=count+n/5;
