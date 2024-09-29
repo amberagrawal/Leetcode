1862e5b94af96bd2d50f329dfa942c162089767a
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-29 13:17:03 +0530

    Time: 17 ms (63.64%), Space: 8.9 MB (72.73%) - LeetHub

---
+++ 3570-count-of-substrings-containing-every-vowel-and-k-consonants-i/3570-count-of-substrings-containing-every-vowel-and-k-consonants-i.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countOfSubstrings(string word, int k) {
+        int count=0;
+        for(int it=0;it<word.length();it++){
+            int a=0;
+            int e=0;
+            int i=0;
+            int o=0;
+            int u=0;
+            int c=0;
+            for(int j=it;j<word.length();j++){
+                if(word[j]=='a'){
+                    a++;
+                }
+                else if(word[j]=='e'){
+                    e++;
+                }
+                else if(word[j]=='i'){
+                    i++;
+                }
+                else if(word[j]=='o'){
+                    o++;
+                }
+                else if(word[j]=='u'){
+                    u++;
+                }
+                else{
+                    c++;
+                }
+                if(a>0&&e>0&&i>0&&o>0&&u>0&&c==k){
+                count++;
+                }
+            }
+        }
+        return count;
+    }
+};
