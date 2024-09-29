f6ea3a5d2c7914f0cf26692d6f0fb3f6560d598d
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-30 01:01:31 +0530

    Time: 150 ms (41.36%), Space: 76.6 MB (12.26%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
+      ios::sync_with_stdio(false);
+      cin.tie(nullptr);
        map<int,int>mp;
        for(int i=0;i<nums.size();i++){
         mp[nums[i]]++;
