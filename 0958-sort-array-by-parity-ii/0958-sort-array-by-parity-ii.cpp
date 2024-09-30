1ff053478cd3f7d76ec05d87d457d7c110ccae8c
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-09-30 18:09:05 +0530

    Time: 11 ms (82.37%), Space: 25.7 MB (24.34%) - LeetHub

---
+++ 0958-sort-array-by-parity-ii/0958-sort-array-by-parity-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> sortArrayByParityII(vector<int>& nums) {
+       vector<int>e;
+       vector<int>o;
+       for(int i=0;i<nums.size();i++){
+        if(nums[i]%2==0){
+            e.push_back(nums[i]);
+        }
+        else{
+            o.push_back(nums[i]);
+        }
+       } 
+       int p=0;
+       int q=0;
+       for(int i=0;i<nums.size();i++){
+        if(i%2==0){
+            nums[i]=e[p];
+            p++;
+        }
+        else{
+            nums[i]=o[q];
+            q++;
+        }
+       }
+       return nums;
+    }
+};
