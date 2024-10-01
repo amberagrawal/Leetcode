71b2678bc6567a73dceddfecb4503588c7eb9263
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-01 20:28:36 +0530

    Time: 200 ms (5.09%), Space: 60.1 MB (6.85%) - LeetHub

---
+++ 1112-find-words-that-can-be-formed-by-characters/1112-find-words-that-can-be-formed-by-characters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int countCharacters(vector<string>& words, string chars) {
+       map<char,int>mp1;
+       int total=0;
+       for(int i=0;i<chars.size();i++){
+        mp1[chars[i]]++;
+       }
+       for(int i=0;i<words.size();i++){
+        string s=words[i];
+        map<char,int>mp2;
+        for(int j=0;j<s.length();j++){
+            mp2[s[j]]++;
+        }
+        bool check=true;
+        for(auto it:mp2){
+            if(mp1[it.first]<it.second){
+                check=false;
+                break;
+            }
+        }
+        if(check){
+            total=total+s.length();
+        }
+       }
+       return total;
+    }
+};
