8b0ae2eb6cbb5d4be89c55922dbebf2f75f3247f
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-02 00:00:27 +0530

    Time: 100 ms (17.43%), Space: 26 MB (64.36%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.cpp
@@ -1,8 +1,13 @@
 class Solution {
 public:
     vector<string> findRepeatedDnaSequences(string s) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         map<string,int>mp;
         vector<string>v;
+        if(s.size()<10){
+            return v;
+        }
        for(int i=0;i<=s.length()-10;i++){
         string s1=s.substr(i,10);
         mp[s1]++;
