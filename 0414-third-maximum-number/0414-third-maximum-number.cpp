8b3f65e37e2b008da68b6ef8552e97a85be5a6bb
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-01 18:15:07 +0530

    Time: 9 ms (21.54%), Space: 16.7 MB (5.74%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         sort(nums.rbegin(),nums.rend());
         set<int>s(nums.begin(),nums.end());
         if(s.size()<3){
