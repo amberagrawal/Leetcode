074518f84b4abc543cfe35db0e2a8b5dc69083be
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-03 20:29:59 +0530

    Time: 115 ms (22.42%), Space: 21.9 MB (89.61%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -2,14 +2,10 @@ class NumArray {
 public:
     vector<int>v;
     NumArray(vector<int>& nums) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
         v=nums;
     }
     
     int sumRange(int left, int right) {
-        ios::sync_with_stdio(false);
-        cin.tie(nullptr);
         int sum=0;
         for(int i=left;i<=right;i++){
             sum=sum+v[i];
