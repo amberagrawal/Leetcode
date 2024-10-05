69a4829ff1635b7566f644145ccd22e1b968c635
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-05 13:43:36 +0530

    Time: 1810 ms (5.06%), Space: 181.2 MB (5.01%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        map<string,int>mp;
+        if(s2.length()<s1.length()){
+            return false;
+        }
+            for(int i=0;i<=s2.length()-s1.length();i++){
+                string s3=s2.substr(i,s1.length());
+                sort(s3.begin(),s3.end());
+                mp[s3]++;
+            }
+        sort(s1.begin(),s1.end());
+        if(mp[s1]>0){
+            return true;
+        }
+        return false;
+    }
+};
