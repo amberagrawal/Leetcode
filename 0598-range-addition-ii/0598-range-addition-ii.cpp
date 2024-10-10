4519de69989cd06922439e5de8ccca7df8bb79b6
amberagrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-10 22:14:43 +0530

    Time: 4 ms (87.8%), Space: 14.4 MB (64.96%) - LeetHub

--- 0598-range-addition-ii/0598-range-addition-ii.cpp
+++ 0598-range-addition-ii/0598-range-addition-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxCount(int m, int n, vector<vector<int>>& ops) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         int a=m;
         for(int i=0;i<ops.size();i++){
             a=min(a,ops[i][0]);
