96b85cfef04891f236d7cd611e10c8102df6d2f7
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-22 22:19:06 +0530

    Time: 53 ms (14.9%), Space: 40.4 MB (8.25%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        map<int, int> mp;
-        mp[0] = 1;
-        int sum = 0;
-        int count = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            sum += nums[i];
-            if (mp.find(sum - goal) != mp.end()) {
-                count += mp[sum - goal];
-            }
-            mp[sum]++;
+        vector<int>v(nums.size(),0);
+        v[0]=nums[0];
+        for(int i=1;i<v.size();i++){
+            v[i]=v[i-1]+nums[i];
         }
-
+        map<int,int>mp;
+        mp[0]=1;
+        int count=0;
+        for(int i=0;i<v.size();i++){
+            int a=v[i]-goal;
+            if(mp.find(a)!=mp.end()){
+                count=count+mp[a];
+            }
+            mp[v[i]]++;
+        }
         return count;
     }
 };
