499c48d974c2863ff60c01035dfaf59d2cb3671e
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2024-10-30 14:21:11 +0530

    Time: 47 ms (10%), Space: 80.4 MB (23.54%) - LeetHub

--- 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
+++ 1961-maximum-ice-cream-bars/1961-maximum-ice-cream-bars.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
+        ios::sync_with_stdio(false);
+        cin.tie(nullptr);
         sort(costs.begin(),costs.end());
         int count=0;
         for(int i=0;i<costs.size();i++){
