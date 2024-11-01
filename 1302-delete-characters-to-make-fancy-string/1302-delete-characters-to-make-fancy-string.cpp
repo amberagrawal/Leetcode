3b8a3f4cc53d1b9c6880a42d2692613eb1003538
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2024-11-01 20:22:50 +0530

    Time: 32 ms (38.96%), Space: 42.8 MB (63.26%) - LeetHub

--- 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
+++ 1302-delete-characters-to-make-fancy-string/1302-delete-characters-to-make-fancy-string.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     string makeFancyString(string s) {
-        string result;
-        int count = 1; // To count consecutive identical characters
-        result += s[0]; // Initialize the result with the first character
-
-        for (int i = 1; i < s.length(); i++) {
-            if (s[i] == s[i - 1]) {
+        string s1;
+        int count=1;
+        s1+=s[0];
+        for(int i=1;i<s.length();i++){
+            if(s[i]==s[i-1]){
                 count++;
-            } else {
-                count = 1; // Reset count for a new character
             }
-
-            // Only add the character if the count is less than 3
-            if (count < 3) {
-                result += s[i];
+            else{
+                count=1;
+            }
+            if(count<3){
+                s1+=s[i];
             }
         }
-
-        return result;
+        return s1;
     }
 };
