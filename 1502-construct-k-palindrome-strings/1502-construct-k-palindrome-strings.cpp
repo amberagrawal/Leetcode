c527cf53c8120321a92ac3f97951a4faf9c660ee
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2025-01-11 13:19:07 +0530

    Time: 21 ms (17.13%), Space: 15 MB (33.92%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool canConstruct(string s, int k) {
+        map<char,int>mp;
+        for(int i=0;i<s.length();i++){
+            mp[s[i]]++;
+        }
+        int odd=0;
+        for(auto i:mp){
+            if(i.second%2!=0){
+                odd++;
+            }
+        }
+        if(s.length()<k){
+            return false;
+        }
+        if(odd>k){
+            return false;
+        }
+        return true;
+    }
+};
