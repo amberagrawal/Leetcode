a4c00a3b9d5ce42c90e4056b8d0d5c268d0e0ab7
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2025-09-20 19:09:50 +0530

    Time: 3 ms (76.88%), Space: 29.4 MB (31.49%) - LeetHub

--- 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.cpp
+++ 3997-maximize-sum-of-at-most-k-distinct-elements/3997-maximize-sum-of-at-most-k-distinct-elements.cpp
@@ -3,7 +3,7 @@ public:
     vector<int> maxKDistinct(vector<int>& nums, int k) {
         vector<int>v;
         set<int>s(nums.begin(),nums.end());
-        for (auto it = s.rbegin(); it != s.rend(); ++it){
+        for (auto it = s.rbegin(); it != s.rend(); it++){
             v.push_back(*it);
             if(v.size()==k){
                 break;
