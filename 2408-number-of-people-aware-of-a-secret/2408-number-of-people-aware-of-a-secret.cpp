895cc2e07643957192cce80458ed3f75fea3903f
Amber Agrawal <148850368+amberagrawal@users.noreply.github.com>
2025-09-20 15:17:55 +0530

    Time: 77 ms (5.02%), Space: 9.9 MB (16.21%) - LeetHub

---
+++ 2408-number-of-people-aware-of-a-secret/2408-number-of-people-aware-of-a-secret.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    const int mod=1e9+7;
+    int peopleAwareOfSecret(int n, int d, int f) {
+        queue<pair<int,int>>res;
+        int act=0;
+        queue<pair<int,int>>frg;
+        res.push({1,d+1});
+        frg.push({1,f+1});
+        for(int i=1;i<=n;i++){
+            while(!frg.empty()&&frg.front().second==i){
+                act=(act-frg.front().first+mod)%mod;
+                frg.pop();
+            }
+            while(!res.empty()&&res.front().second==i){
+                act=(act+res.front().first)%mod;
+                res.pop();
+            }
+            if(act>0){
+            res.push({act,i+d});
+            frg.push({act,i+f});
+            }
+            cout<<act+res.size()<<" ";
+        }
+        while(!res.empty()){
+            act=(act+res.front().first)%mod;
+            res.pop();
+        }
+        return act;
+    }
+};
